a731647056c44dda21ae4014b7123c048394ff87
Jason Ford <fordjason@gmail.com>
2023-09-18 12:54:20 -0700

    Make more illustrative entry point in embedding example

--- examples/embedding/index.js
+++ examples/embedding/index.js
@@ -4,12 +4,22 @@ import { createApp } from 'vue'
 import root from './index.vue'
 
 window.Agent = browserAgent()
+
 const url = new URL(window.location.href)
 const { pathname } = url
 
-// pathname is always prefixed with '/', so here we remove it
-const { context } = await Agent.environment()
+// If path is a uuid, use the corresponding data as props to the root component
+const id = pathname.slice(1)
+const props = isUUID(id) ? await Agent.state(id) : {}
 
+console.log('LOADED SOME EXPERIENCE!!!!', props)
+
+//  Use the serialized embedding context as the default scope for
+//  this experience. Another possible choice is to simply use the
+//  last part of the context as the name if reading/writing the same
+//  scope in any context is the desired behavior for this content.
+const { context } = await Agent.environment()
 const defaultScope = '/' + context.join('/')
-const app = createApp(vuePersistentComponent(root, defaultScope))
+
+const app = createApp(vuePersistentComponent(root, defaultScope), props)
 app.mount(document.body)
