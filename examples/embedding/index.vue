dd0c4d60f3a3de805d93e1bc11cbff8ff9c48444
Jason Ford <fordjason@gmail.com>
2023-09-18 13:56:53 -0700

    Add type editor scaffold to embed example

--- examples/embedding/index.vue
+++ examples/embedding/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div v-if="environment" class="wrapper">
+  <div v-if="type">
+    Some {{ type }} Question!!!!
+  </div>
+  <div v-else-if="environment" class="wrapper">
     <h2>Context</h2>
     {{ '/' + environment.context.join('/') }}
     <h2>Children</h2>
@@ -10,15 +13,25 @@
           :key="id"
           :class="{ selected: current === id }"
         >
-          <td @click="current = current === id ? null : id">{{id}}</td>
+          <td @click="select(id)">{{id}}</td>
           <td><button @click="removeTake(index)">x</button></td>
         </tr>
       </tbody>
     </table>
-    <button @click="createContent">new</button>
+    <button
+      v-for="type in types"
+      :key="type"
+      @click="startCreating(type)"
+    >
+      new {{ type }}
+    </button>
     <button @click="openTest">test</button>
+    <div v-if="creating">
+      TODO: help create! {{ creating }}
+      <button @click="create">create</button>
+    </div>
     <vueContentComponent
-      v-if="current"
+      v-else-if="current"
       :key="current"
       :id="current"
     />
@@ -31,10 +44,22 @@
 
   export default {
     components: { vueContentComponent },
+    props: {
+      type: {
+        type: String,
+        required: false
+      }
+    },
     data() {
       return {
         current: null,
         environment: null,
+        types: [
+          'multiple choice',
+          'free response',
+          'rating'
+        ],
+        creating: null,
         children: []
       }
     },
@@ -42,23 +67,24 @@
       this.environment = await Agent.environment()
     },
     methods: {
-      async createContent() {
+      async startCreating(type) {
+        this.current = null
+        this.creating = type === this.creating ? null : type
+      },
+      async create() {
         const id = await Agent.create({
-          active_type: 'application/json;type=demo-stats',
-          active: {
-            time: 0,
-            progress: 0,
-            score: 0,
-            success: null,
-            scope: uuid(),
-            content: null,
-            parent: null
-          }
+          active: { type: this.creating }
         })
+        this.creating = null
         this.children.push(id)
+        this.current = id
+      },
+      select(id) {
+        this.creating = null
+        this.current = this.current === id ? null : id
       },
       openTest() {
-        this.children.push('https://localhost:5174')
+        this.children.push('https://test.knowlearning.systems')
       },
       removeTake(index) {
         if (this.children[index] === this.current) this.current = null
