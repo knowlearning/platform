ec0f2ec83246fe19173a6daaed36fd19f1025674
Jason Ford <fordjason@gmail.com>
2023-09-19 16:32:32 -0700

    Make name draggable

--- examples/embedding/components/name.vue
+++ examples/embedding/components/name.vue
@@ -1,21 +1,37 @@
 <template>
-  <span>
-  	{{ metadata ? metadata.name.trim() || 'unnamed' : '...' }}
+  <span
+    draggable="true"
+    @dragstart="handleDragStart"
+  >
+    {{ metadata ? metadata.name.trim() || 'unnamed' : '...' }}
   </span>
 </template>
 
 <script>
   export default {
-  	props: {
-  	  id: String
-  	},
-  	data() {
-  	  return {
-  	  	metadata: null
-  	  }
-  	},
-  	async created() {
-  	  this.metadata = await Agent.metadata(this.id)
-  	}
+    props: {
+      id: String
+    },
+    data() {
+      return {
+        metadata: null
+      }
+    },
+    async created() {
+      this.metadata = await Agent.metadata(this.id)
+    },
+    methods: {
+      handleDragStart(e) {
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/plain', this.id);
+      }
+    }
   }
 </script>
+
+<style>
+  span
+  {
+    cursor: grab;
+  }
+</style>
