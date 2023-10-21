574ccff6c7e535c79d7146dee4b01f68e12a8463
Jason Ford <fordjason@gmail.com>
2023-10-21 15:50:43 -0700

    Test short-circuiting deep watchers

--- test/tests/watch.js
+++ test/tests/watch.js
@@ -51,10 +51,7 @@ export default function () {
       'Can watch references inside of scopes',
       async function () {
         const id = uuid()
-        await Agent.create({
-          id,
-          active: { x: { y: { z: 100 } } }
-        })
+        await Agent.create({ id, active: { x: { y: { z: 100 } } } })
         const expectedValues = [100, 200, 300, 400]
         const seenValues = []
 
@@ -73,20 +70,13 @@ export default function () {
     )
 
     it(
-      'Can can watch references embedded 1 level within a scope',
+      'Can watch references embedded 1 level within a scope',
       async function () {
         const id1 = uuid()
         const id2 = uuid()
 
-        await Agent.create({
-          id: id1,
-          active: { id_referencing_other_scope: id2 }
-        })
-
-        await Agent.create({
-          id: id2,
-          active: { x: 'woooo!' }
-        })
+        await Agent.create({ id: id1, active: { id_referencing_other_scope: id2 } })
+        await Agent.create({ id: id2, active: { x: 'woooo!' } })
 
         const expectedValues = ['woooo!', 'wooooooooooo!x2']
         const seenValues = []
@@ -102,29 +92,16 @@ export default function () {
       }
     )
 
-
-
     it(
-      'Can can watch references embedded 2 levels within scopes',
+      'Can watch references embedded 2 levels within scopes',
       async function () {
         const id1 = uuid()
         const id2 = uuid()
         const id3 = uuid()
 
-        await Agent.create({
-          id: id1,
-          active: { id_referencing_other_scope: id2 }
-        })
-
-        await Agent.create({
-          id: id2,
-          active: { id_referencing_other_other_scope: id3 }
-        })
-
-        await Agent.create({
-          id: id3,
-          active: { x: 'woooo!' }
-        })
+        await Agent.create({ id: id1, active: { id_referencing_other_scope: id2 } })
+        await Agent.create({ id: id2, active: { id_referencing_other_other_scope: id3 } })
+        await Agent.create({ id: id3, active: { x: 'woooo!' } })
 
         const expectedValues = ['woooo!', 'wooooooooooo!x2']
         const seenValues = []
@@ -135,6 +112,37 @@ export default function () {
         Agent.watch([id1, 'id_referencing_other_scope', 'id_referencing_other_other_scope', 'x'], v => {
           seenValues.push(v)
           if (seenValues.length === 1) id3State.x = 'wooooooooooo!x2'
+        })
+
+        await new Promise(r => setTimeout(r, 50))
+
+        expect(seenValues).to.deep.equal(expectedValues)
+      }
+    )
+
+    it(
+      'Can short circuit deep watchers',
+      async function () {
+        const id1 = uuid()
+        const id2 = uuid()
+        const id3 = uuid()
+
+        await Agent.create({ id: id1, active: { id_referencing_other_scope: id2 } })
+        await Agent.create({ id: id2, active: { reference_to_short_circuit: id3 } })
+        await Agent.create({ id: id3, active: { x: 'woooo!' } })
+
+        const expectedValues = ['woooo!', 'bye bye!', 'zap!']
+        const seenValues = []
+
+        const id2State = await Agent.state(id2)
+        const id3State = await Agent.state(id3)
+
+        Agent.watch([id1, 'id_referencing_other_scope', 'reference_to_short_circuit', 'x'], v => {
+          seenValues.push(v)
+          if (seenValues.length === 1) {
+            id3State.x = 'bye bye!'
+            id2State.reference_to_short_circuit = { x: 'zap!' }
+          }
         })
 
         await new Promise(r => setTimeout(r, 50))
