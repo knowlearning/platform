931aaf6b3d18d21e7a779fcff7f4b0f18f702b01
Jason Ford <fordjason@gmail.com>
2023-11-07 11:18:13 -0800

    Modularize message queue for agent

--- client/agents/generic/index.js
+++ client/agents/generic/index.js
@@ -1,65 +1,44 @@
 import MutableProxy from '../../persistence/json.js'
+import initializeMessageQueue from './initialize-message-queue.js'
 import stateImplementation from './state.js'
 import downloadImplementation from '../download.js'
 
 // TODO: consider using something better than name as mechanism
 //       for resoling default scope in context
 const DEFAULT_SCOPE_NAME = '[]'
-const HEARTBEAT_TIMEOUT = 10000
 const SESSION_TYPE = 'application/json;type=session'
 const UPLOAD_TYPE = 'application/json;type=upload'
 const POSTGRES_QUERY_TYPE = 'application/json;type=postgres-query'
 const TAG_TYPE = 'application/json;type=tag'
 const DOMAIN_CLAIM_TYPE = 'application/json;type=domain-claim'
 
-//  transform our custom path implementation to the standard JSONPatch path
-function standardJSONPatch(patch) {
-  return patch.map(p => {
-    return {...p, path: '/' + p.path.map(sanitizeJSONPatchPathSegment).join('/')}
-  })
-}
-
 function isUUID(string) {
   return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(string)
 }
 
-function sanitizeJSONPatchPathSegment(s) {
-  if (typeof s === "string") return s.replaceAll('~', '~0').replaceAll('/', '~1')
-  else return s
-}
-
-const sessionMetrics = {
-  loaded: Date.now(),
-  connected: null,
-  authenticated: null
-}
-
 export default function Agent({ host, token, WebSocket, protocol='ws', uuid, fetch, applyPatch, login, logout, reboot }) {
-  let ws
   let user
-  let domain
   let session
-  let server
-  let isSynced = false
-  let si = -1
-  let authed = false
   const states = {}
-  const responses = {}
   const watchers = {}
   const keyToSubscriptionId = {}
   const lastInteractionResponse = {}
   const tagTypeToTargetCache = {}
-  const messageQueue = []
   let resolveEnvironment
-  let disconnected = false
-  let failedConnections = 0
   let mode = 'normal'
   const environmentPromise = new Promise(r => resolveEnvironment = r)
-  let lastSentSI = -1
-  let lastHeartbeat
-  let lastSynchronousScopePatched = null
-  let lastSynchronousScopePatchPromise = null
-  const syncedPromiseResolutions = []
+
+  log('INITIALIZING AGENT CONNECTION')
+  const messageQueueReferences = { token, protocol, host, WebSocket, watchers, states, applyPatch, log, login, interact }
+  const setSession = id => session = id
+  const setUser = id => user = id
+  const [
+    queueMessage,
+    lastMessageResponse,
+    disconnect,
+    reconnect,
+    synced
+  ] = initializeMessageQueue(setSession, setUser, resolveEnvironment, messageQueueReferences)
 
   const internalReferences = {
     keyToSubscriptionId,
@@ -67,207 +46,25 @@ export default function Agent({ host, token, WebSocket, protocol='ws', uuid, fet
     states,
     create,
     session,
-    lastMessageResponse,
     lastInteractionResponse,
+    lastMessageResponse,
     tagIfNotYetTaggedInSession,
     interact,
     fetch,
-    metadata
+    metadata,
   }
 
-  log('INITIALIZING AGENT CONNECTION')
-  initWS()
+  function state(scope) { return stateImplementation(scope, internalReferences) }
 
-  function state(scope) {
-    return stateImplementation(scope, internalReferences)
-  }
+  function download(id) { return downloadImplementation(id, internalReferences) }
 
-  function download(id) {
-    return downloadImplementation(id, internalReferences)
-  }
-
-  function log() {
-    if (mode === 'debug') console.log(...arguments)
-  }
-
-  function resolveSyncPromises() {
-    while (syncedPromiseResolutions.length) syncedPromiseResolutions.shift()()
-    isSynced = true
-  }
+  function log() { if (mode === 'debug') console.log(...arguments) }
 
   function removeWatcher(key, fn) {
     const watcherIndex = watchers[key].findIndex(x => x === fn)
     if (watcherIndex > -1) watchers[key].splice(watcherIndex, 1)
     else console.warn('TRIED TO REMOVE WATCHER THAT DOES NOT EXIST')
   }
-
-  // TODO: clear acknowledged messages
-  async function flushMessageQueue() {
-    // this makes flushing async, giving time for queue message to combine synchronous updates
-    await new Promise(resolve => resolve())
-    lastSynchronousScopePatched = null
-
-    while (authed && ws.readyState === WebSocket.OPEN && lastSentSI+1 < messageQueue.length) {
-      lastSynchronousScopePatched = null
-      lastSentSI += 1
-      ws.send(JSON.stringify(messageQueue[lastSentSI]))
-
-      //  async so we don't try and push more to a closed connection
-      await new Promise(resolve => resolve())
-    }
-  }
-
-  function lastMessageResponse() { //  TODO: handle error responses
-    return new Promise((resolve, reject) => responses[si].push([resolve, reject]))
-  }
-
-  function queueMessage({ scope, patch }) {
-    isSynced = false
-    if (lastSynchronousScopePatched === scope) {
-      const i = messageQueue.length - 1
-      messageQueue[i].patch = [...messageQueue[i].patch, ...patch]
-    }
-    else {
-      si += 1
-      lastSynchronousScopePatchPromise = new Promise((resolve, reject) => responses[si] = [[resolve, reject]])
-      messageQueue.push({ scope, patch, si, ts: Date.now()})
-      lastSynchronousScopePatched = scope
-      flushMessageQueue()
-    }
-
-    return lastSynchronousScopePatchPromise
-  }
-
-  function checkHeartbeat() {
-    clearTimeout(lastHeartbeat)
-    lastHeartbeat = setTimeout(
-      () => {
-        log('CLOSING DUE TO HEARTBEAT TIMEOUT')
-        restartConnection()
-      },
-      HEARTBEAT_TIMEOUT
-    )
-  }
-
-  let restarting = false
-  async function restartConnection() {
-    if (restarting) return
-
-    authed = false
-    if (!disconnected) {
-      await new Promise(r => setTimeout(r, Math.min(1000, failedConnections * 100)))
-      ws.onmessage = () => {} // needs to be a no-op since a closing ws can still get messages
-      restarting = true
-      failedConnections += 1
-      initWS() // TODO: don't do this if we are purposefully unloading...
-      restarting = false
-    }
-  }
-
-  function initWS() {
-    ws = new WebSocket(`${protocol}://${host}`)
-
-    ws.onopen = async () => {
-      if (!sessionMetrics.connected) sessionMetrics.connected = Date.now()
-      log('AUTHORIZING NEWLY OPENED WS FOR SESSION:', session)
-      failedConnections = 0
-      ws.send(JSON.stringify({ token: await token(), session }))
-    }
-
-    ws.onmessage = async ({ data }) => {
-      checkHeartbeat()
-      if (data.length === 0) return // heartbeat
-
-      try {
-        log('handling message', disconnected, authed)
-        const message = JSON.parse(data)
-        if (mode === 'debug') log('message', JSON.stringify(message))
-
-        if (message.error) console.warn('ERROR RESPONSE', message)
-
-        if (!authed) {
-          //  TODO: credential refresh flow instead of forcing login
-          if (message.error) return login()
-
-          authed = true
-          if (!user) { // this is the first authed websocket connection
-            sessionMetrics.authenticated = Date.now()
-            user = message.auth.user
-            session = message.session
-            domain = message.domain
-            server = message.server
-
-            // save session metrics
-            interact(session, [
-              {op: 'add', path: ['active', 'loaded'], value: sessionMetrics.loaded },
-              {op: 'add', path: ['active', 'connected'], value: sessionMetrics.connected },
-              {op: 'add', path: ['active', 'authenticated'], value: sessionMetrics.authenticated },
-            ])
-            resolveEnvironment(message)
-          }
-          else if (server !== message.server) {
-            console.warn(`REBOOTING DUE TO SERVER SWITCH ${server} -> ${message.server}`)
-            reboot()
-          }
-          else {
-            lastSentSI = message.ack
-          }
-          flushMessageQueue()
-        }
-        else {
-          if (message.si !== undefined) {
-            if (responses[message.si]) {
-              //  TODO: remove "acknowledged" messages from queue and do accounting with si
-              responses[message.si]
-                .forEach(([resolve, reject]) => {
-                  message.error ? reject(message) : resolve(message)
-                })
-              delete responses[message.si]
-              ws.send(JSON.stringify({ack: message.si})) //  acknowledgement that we have received the response for this message
-              if (Object.keys(responses).length === 0) {
-                resolveSyncPromises()
-              }
-            }
-            else {
-              //  TODO: consider what to do here... probably want to throw error if in dev env
-              console.warn('received MULTIPLE responses for message with si', message.si, message)
-            }
-          }
-          else {
-            if (watchers[message.scope]) {
-              states[message.scope] = await states[message.scope]
-
-              const lastResetPatchIndex = message.patch.findLastIndex(p => p.path.length === 0)
-              if (lastResetPatchIndex > -1) states[message.scope] = message.patch[lastResetPatchIndex].value
-
-              if (states[message.scope].active === undefined) states[message.scope].active = {}
-              applyPatch(states[message.scope], standardJSONPatch(message.patch.slice(lastResetPatchIndex + 1)))
-              watchers[message.scope]
-                .forEach(fn => {
-                  const state = structuredClone(states[message.scope].active)
-                  fn({ ...message, state })
-                })
-            }
-          }
-        }
-      }
-      catch (error) {
-        console.error('ERROR HANDLING WS MESSAGE', error)
-      }
-    }
-
-    ws.onerror = async error => {
-      log('WS CONNECTION ERROR', error.message)
-    }
-
-    ws.onclose = async error => {
-      log('WS CLOSURE', error.message)
-      restartConnection()
-    }
-
-    checkHeartbeat()
-  }
-
   function create({ id=uuid(), active_type, active, name }) {
     //  TODO: collapse into 1 patch and 1 interact call
     //        (requires updating side effects)
@@ -446,22 +243,6 @@ export default function Agent({ host, token, WebSocket, protocol='ws', uuid, fet
       })
       interact(id, activePatch)
     })
-  }
-
-  async function synced() {
-    return isSynced ? null : new Promise(resolve => syncedPromiseResolutions.push(resolve))
-  }
-
-  function disconnect() {
-    log('DISCONNECTED AGENT!!!!!!!!!!!!!!!')
-    disconnected = true
-    ws.close()
-  }
-
-  function reconnect() {
-    log('RECONNECTED AGENT!!!!!!!!!!!!!!!')
-    disconnected = false
-    restartConnection()
   }
 
   function debug() {
---
+++ client/agents/generic/initialize-message-queue.js
@@ -0,0 +1,233 @@
+const HEARTBEAT_TIMEOUT = 10000
+
+//  transform our custom path implementation to the standard JSONPatch path
+function standardJSONPatch(patch) {
+  return patch.map(p => {
+    return {...p, path: '/' + p.path.map(sanitizeJSONPatchPathSegment).join('/')}
+  })
+}
+
+function sanitizeJSONPatchPathSegment(s) {
+  if (typeof s === "string") return s.replaceAll('~', '~0').replaceAll('/', '~1')
+  else return s
+}
+
+export default function initializeMessageQueue(setSession, setUser, setEnvironment, { token, protocol, host, WebSocket, watchers, states, applyPatch, log, login, interact }) {
+  let ws
+  let user
+  let authed = false
+  let isSynced = false
+  let session
+  let server
+  let si = -1
+  let failedConnections = 0
+  const messageQueue = []
+  let lastSentSI = -1
+  let lastHeartbeat
+  let lastSynchronousScopePatched = null
+  let lastSynchronousScopePatchPromise = null
+  let restarting = false
+  let disconnected = false
+  const syncedPromiseResolutions = []
+  const responses = {}
+
+
+  const sessionMetrics = {
+    loaded: Date.now(),
+    connected: null,
+    authenticated: null
+  }
+
+  function queueMessage({ scope, patch }) {
+    isSynced = false
+    if (lastSynchronousScopePatched === scope) {
+      const i = messageQueue.length - 1
+      messageQueue[i].patch = [...messageQueue[i].patch, ...patch]
+    }
+    else {
+      si += 1
+      lastSynchronousScopePatchPromise = new Promise((resolve, reject) => responses[si] = [[resolve, reject]])
+      messageQueue.push({ scope, patch, si, ts: Date.now()})
+      lastSynchronousScopePatched = scope
+      flushMessageQueue()
+    }
+
+    return lastSynchronousScopePatchPromise
+  }
+
+  // TODO: clear acknowledged messages
+  async function flushMessageQueue() {
+    // this makes flushing async, giving time for queue message to combine synchronous updates
+    await new Promise(resolve => resolve())
+    lastSynchronousScopePatched = null
+
+    while (authed && ws.readyState === WebSocket.OPEN && lastSentSI+1 < messageQueue.length) {
+      lastSynchronousScopePatched = null
+      lastSentSI += 1
+      ws.send(JSON.stringify(messageQueue[lastSentSI]))
+
+      //  async so we don't try and push more to a closed connection
+      await new Promise(resolve => resolve())
+    }
+  }
+
+  function resolveSyncPromises() {
+    while (syncedPromiseResolutions.length) syncedPromiseResolutions.shift()()
+    isSynced = true
+  }
+
+  function checkHeartbeat() {
+    clearTimeout(lastHeartbeat)
+    lastHeartbeat = setTimeout(
+      () => {
+        log('CLOSING DUE TO HEARTBEAT TIMEOUT')
+        restartConnection()
+      },
+      HEARTBEAT_TIMEOUT
+    )
+  }
+
+  async function restartConnection() {
+    if (restarting) return
+
+    authed = false
+    if (!disconnected) {
+      await new Promise(r => setTimeout(r, Math.min(1000, failedConnections * 100)))
+      ws.onmessage = () => {} // needs to be a no-op since a closing ws can still get messages
+      restarting = true
+      failedConnections += 1
+      initWS() // TODO: don't do this if we are purposefully unloading...
+      restarting = false
+    }
+  }
+
+  function initWS() {
+    ws = new WebSocket(`${protocol}://${host}`)
+
+    ws.onopen = async () => {
+      if (!sessionMetrics.connected) sessionMetrics.connected = Date.now()
+      log('AUTHORIZING NEWLY OPENED WS FOR SESSION:', session)
+      failedConnections = 0
+      ws.send(JSON.stringify({ token: await token(), session }))
+    }
+
+    ws.onmessage = async ({ data }) => {
+      checkHeartbeat()
+      if (data.length === 0) return // heartbeat
+
+      try {
+        log('handling message', disconnected, authed)
+        const message = JSON.parse(data)
+        log('message', JSON.stringify(message))
+
+        if (message.error) console.warn('ERROR RESPONSE', message)
+
+        if (!authed) {
+          //  TODO: credential refresh flow instead of forcing login
+          if (message.error) return login()
+
+          authed = true
+          if (!user) { // this is the first authed websocket connection
+            sessionMetrics.authenticated = Date.now()
+            user = message.auth.user
+            session = message.session
+            server = message.server
+
+            // save session metrics
+            interact(session, [
+              {op: 'add', path: ['active', 'loaded'], value: sessionMetrics.loaded },
+              {op: 'add', path: ['active', 'connected'], value: sessionMetrics.connected },
+              {op: 'add', path: ['active', 'authenticated'], value: sessionMetrics.authenticated },
+            ])
+
+            setSession(session)
+            setUser(user)
+            setEnvironment(message)
+          }
+          else if (server !== message.server) {
+            console.warn(`REBOOTING DUE TO SERVER SWITCH ${server} -> ${message.server}`)
+            reboot()
+          }
+          else {
+            lastSentSI = message.ack
+          }
+          flushMessageQueue()
+        }
+        else {
+          if (message.si !== undefined) {
+            if (responses[message.si]) {
+              //  TODO: remove "acknowledged" messages from queue and do accounting with si
+              responses[message.si]
+                .forEach(([resolve, reject]) => {
+                  message.error ? reject(message) : resolve(message)
+                })
+              delete responses[message.si]
+              ws.send(JSON.stringify({ack: message.si})) //  acknowledgement that we have received the response for this message
+              if (Object.keys(responses).length === 0) {
+                resolveSyncPromises()
+              }
+            }
+            else {
+              //  TODO: consider what to do here... probably want to throw error if in dev env
+              console.warn('received MULTIPLE responses for message with si', message.si, message)
+            }
+          }
+          else {
+            if (watchers[message.scope]) {
+              states[message.scope] = await states[message.scope]
+
+              const lastResetPatchIndex = message.patch.findLastIndex(p => p.path.length === 0)
+              if (lastResetPatchIndex > -1) states[message.scope] = message.patch[lastResetPatchIndex].value
+
+              if (states[message.scope].active === undefined) states[message.scope].active = {}
+              applyPatch(states[message.scope], standardJSONPatch(message.patch.slice(lastResetPatchIndex + 1)))
+              watchers[message.scope]
+                .forEach(fn => {
+                  const state = structuredClone(states[message.scope].active)
+                  fn({ ...message, state })
+                })
+            }
+          }
+        }
+      }
+      catch (error) {
+        console.error('ERROR HANDLING WS MESSAGE', error)
+      }
+    }
+
+    ws.onerror = async error => {
+      log('WS CONNECTION ERROR', error.message)
+    }
+
+    ws.onclose = async error => {
+      log('WS CLOSURE', error.message)
+      restartConnection()
+    }
+
+    checkHeartbeat()
+  }
+
+  async function synced() {
+    return isSynced ? null : new Promise(resolve => syncedPromiseResolutions.push(resolve))
+  }
+
+  function lastMessageResponse() { //  TODO: handle error responses
+    return new Promise((resolve, reject) => responses[si].push([resolve, reject]))
+  }
+
+  function disconnect() {
+    log('DISCONNECTED AGENT!!!!!!!!!!!!!!!')
+    disconnected = true
+    ws.close()
+  }
+
+  function reconnect() {
+    log('RECONNECTED AGENT!!!!!!!!!!!!!!!')
+    disconnected = false
+    restartConnection()
+  }
+
+  initWS()
+
+  return [queueMessage, lastMessageResponse, disconnect, reconnect, synced]
+}
