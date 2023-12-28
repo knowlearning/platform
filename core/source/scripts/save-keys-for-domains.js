bbce26410c518435d95b37ee3cfa04081e685b3f
Jason Ford <fordjason@gmail.com>
2023-12-28 14:56:54 -0800

    Fix domain and redis issues

--- core/source/scripts/save-keys-for-domains.js
+++ core/source/scripts/save-keys-for-domains.js
@@ -2,15 +2,14 @@ import * as redis from '../redis.js'
 import scanRedis from './scan-redis.js'
 
 const localhostRegex = /^(?:[a-zA-Z0-9-]+\.)*localhost(?::\d+)?$/
-const domainRegex = /^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$/;
+const domainRegex = /^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,7}$/;
 const isValidDomain = d => localhostRegex.test(d) || domainRegex.test(d)
 
 scanRedis('0', '*', 1000, async key => {
-  const domain = await redis.client.json.get(key, { path: [`$.domain`] })
+  const [domain] = await redis.client.json.get(key, { path: [`$.domain`] })
   if (isValidDomain(domain)) {
-    redis.client.sendCommand(['sadd', domain, key])
-    redis.client.sendCommand(['sadd', 'domains', domain])
-    await multi.exec()
+    await redis.client.sendCommand(['sadd', domain, key])
+    await redis.client.sendCommand(['sadd', 'domains', domain])
   }
   else console.warn('INVALID DOMAIN', key, domain)
 })
