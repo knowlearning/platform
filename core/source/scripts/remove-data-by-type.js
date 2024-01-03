38a38ff20a49131953aca4204e80735342cf0391
Jason Ford <fordjason@gmail.com>
2024-01-03 11:17:25 -0800

    Fix script

--- core/source/scripts/remove-data-by-type.js
+++ core/source/scripts/remove-data-by-type.js
@@ -4,7 +4,7 @@ import scanRedis from './scan-redis.js'
 const TYPE_TO_DELETE = 'application/json;type=postgres-query'
 
 //  delete domain data keys
-scanKeys('0', '*', 1000, async function (key) {
+scanRedis('0', '*', 1000, async function (key) {
   const [ type ] = await redis.client.json.get(key, { path: [`$.active_type`] })
   if (type === TYPE_TO_DELETE) await redis.client.del(key)
 })
