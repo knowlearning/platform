6c68005bca2f4adfaceeda1758d8dc5f168d3fca
Jason Ford <fordjason@gmail.com>
2024-01-03 11:08:31 -0800

    Run remove by type script

--- core/source/index.js
+++ core/source/index.js
@@ -9,7 +9,7 @@ import handleHTTP from './handle-http.js'
 import { applyConfiguration, ensureDomainConfigured } from './side-effects/configure.js'
 import ADMIN_DOMAIN_CONFIG from './admin-domain-config.js'
 import compressionLoop from './compress/loop.js'
-import './scripts/record-domain-sizes.js'
+import './scripts/remove-data-by-type.js'
 
 const {
   MODE,
