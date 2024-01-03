994b6d47e140af094506a81d57cd726fa6252786
Jason Ford <fordjason@gmail.com>
2024-01-03 10:57:27 -0800

    Fix type in scrypt

--- core/source/scripts/record-domain-sizes.js
+++ core/source/scripts/record-domain-sizes.js
@@ -7,7 +7,7 @@ const domainSizes = {}
 scanRedis('0', '*', 1000, async key => {
   const [domain] = await redis.client.json.get(key, { path: [`$.domain`] })
   if (!domainSizes[domain]) domainSizes[domain] = 0
-  domainSizes[domain] += (await redis.client.sendCommand(['JSON.DEBUG', 'MEMORY', key]))[0] || 0
+  domainSizes[domain] += parseInt(await redis.client.sendCommand(['JSON.DEBUG', 'MEMORY', key])) || 0
 })
 .then(() => {
   console.log('DOMAIN SIZES', domainSizes)
