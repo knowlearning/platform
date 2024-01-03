a1dbd4762a27b9ad07796d8feb6f7a52945d6214
Jason Ford <fordjason@gmail.com>
2024-01-03 11:07:46 -0800

    Indentation

--- core/source/scripts/remove-data-by-type.js
+++ core/source/scripts/remove-data-by-type.js
@@ -1,8 +1,10 @@
 import * as redis from '../redis.js'
 import scanRedis from './scan-redis.js'
 
+const TYPE_TO_DELETE = 'application/json;type=postgres-query'
+
 //  delete domain data keys
 scanKeys('0', '*', 1000, function (keys) {
-    const [ type ] = await redis.client.json.get(key, { path: [`$.active_type`] })
-    if (type === TYPE_TO_DELETE) await redis.client.del(key)
+  const [ type ] = await redis.client.json.get(key, { path: [`$.active_type`] })
+  if (type === TYPE_TO_DELETE) await redis.client.del(key)
 })
