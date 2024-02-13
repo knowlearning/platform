573cc6b16e296f0dcb06a4f166fabcd16b3b22a1
Jason Ford <fordjason@gmail.com>
2024-02-12 22:09:19 -0800

    Add Connection to deno agent

--- client/deno.js
+++ client/deno.js
@@ -4,10 +4,23 @@ import Agent from './agents/generic/index.js'
 const SERVE_HOST = Deno.env.get("SERVE_HOST")
 const SERVICE_ACCOUNT_TOKEN = Deno.env.get("SERVICE_ACCOUNT_TOKEN")
 
+const postMessage = message => self.postMessage(message)
+
+function Connection() {
+  this.send = postMessage
+
+  (async function () {
+    await new Promise(r => setTimeout(r))
+    this.onopen && this.onopen()
+  })()
+
+  //  TODO: consider what onclose and onerror mean in this case
+  return this
+}
+
 export default new Agent({
-  host: SERVE_HOST,
+  Connection,
   token: () => Deno.readTextFile(SERVICE_ACCOUNT_TOKEN),
-  WebSocket,
   uuid: () => crypto.randomUUID(),
   fetch,
   applyPatch: fastJSONPatch.applyPatch,
