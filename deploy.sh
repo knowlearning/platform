0d1ae2ff6c8db74737d9e9a1d3276940db2650b6
Jason Ford <fordjason@gmail.com>
2024-02-27 15:36:22 -0800

    Add extra guard for deploying to production

--- deploy.sh
+++ deploy.sh
@@ -46,20 +46,26 @@ elif [ $1 = staging ]; then
     exit 1
   fi
 elif [ $1 = production ]; then
-  read -p 'Are you sure you want to deploy to PRODUCTION? (y/N): ' choice
-  if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
-    echo 'Deploying To Live Production Cluster'
-    # old job must be removed before adding new one
-    KUBE_CONTEXT=gke_opensourcelearningplatform_us-central1_skaffold-deployed
-    skaffold run \
-      --profile production \
-      --filename ./skaffold.yaml \
-      --kube-context=$KUBE_CONTEXT \
-      --default-repo=gcr.io/opensourcelearningplatform
-    echo 'Done!'
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+  if [ "$current_branch" = "trunk" ]; then
+    read -p 'Are you sure you want to deploy to PRODUCTION? (y/N): ' choice
+    if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
+      echo 'Deploying To Live Production Cluster'
+      # old job must be removed before adding new one
+      KUBE_CONTEXT=gke_opensourcelearningplatform_us-central1_skaffold-deployed
+      skaffold run \
+        --profile production \
+        --filename ./skaffold.yaml \
+        --kube-context=$KUBE_CONTEXT \
+        --default-repo=gcr.io/opensourcelearningplatform
+      echo 'Done!'
+    else
+      echo 'Deployment aborted'
+      exit 1
+    fi
   else
-    echo 'Deployment aborted'
-    exit 1
+      echo "Must be on trunk branch to deploy to production.\nCurrent branch: $current_branch"
   fi
 else
   echo Unrecognized argument $1
