56c37b6eeda93ffd91772f4d74b4095a0c692fc3
Jason Ford <fordjason@gmail.com>
2024-02-29 12:19:07 -0800

    Remove patch files

--- client/node.js
+++ client/node.js
@@ -1,2 +1,18 @@
-export { default as NodeAgent } from '../agents/node.js'
-export { default as GenericAgent } from '../agents/generic/index.js'
+import fs from 'fs'
+import WebSocket from 'ws'
+import { v1 as uuid } from 'uuid'
+import fastJSONPatch from 'fast-json-patch'
+import fetch from 'node-fetch'
+import Agent from './agents/generic/index.js'
+
+const { SERVE_HOST, SERVICE_ACCOUNT_TOKEN } = process.env
+
+export default new Agent({
+  host: SERVE_HOST,
+  token: () => fs.promises.readFile(SERVICE_ACCOUNT_TOKEN).then(f => f.toString()),
+  WebSocket,
+  uuid,
+  fetch,
+  applyPatch: fastJSONPatch.applyPatch,
+  reboot: () => process.exit(1)
+})
