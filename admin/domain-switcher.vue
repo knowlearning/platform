e7e6320da7a4d2b5cb7ffd6f03e5730128cca15b
Jason Ford <fordjason@gmail.com>
2024-03-12 19:58:14 -0700

    Add domain reomve functionality

--- admin/domain-switcher.vue
+++ admin/domain-switcher.vue
@@ -1,33 +1,25 @@
 <script setup>
-  import { ref, watch } from 'vue'
+  import { ref, computed, watch } from 'vue'
   import { useRouter } from 'vue-router'
 
   const router = useRouter()
 
   const domain = ref(router.currentRoute?.value?.params?.domain || null)
-  const domains = ref(null)
   const domainStates = ref(null)
   const newDomainInput = ref('')
 
   Agent.state('domains').then(state => {
     // add domain to viewed domains if not present
-    if (domain.value && !state[domain.value]) {
-      state[domain.value] = true
-    }
-
-    domains.value = Object.keys(state)
-    if (domain.value && !domains.value.includes(domain.value)) {
-      //  TODO: validate domain name
-      domains.value.push(domain.value)
-    }
+    if (domain.value && !state[domain.value]) state[domain.value] = true
     domainStates.value = state
   })
+
+  const domains = computed(() => Object.keys(domainStates.value || {}))
 
   function addDomain() {
     const name = prompt('Enter domain name')
     if (name) {
       domainStates.value[name] = true
-      domains.value.push(name)
       domain.value = name
       manageDomain(name)
     }
@@ -38,6 +30,10 @@
   }
 
   watch(domain, () => manageDomain(domain.value))
+
+  function removeDomain(domain) {
+    delete domainStates.value[domain]
+  }
 
 </script>
 
@@ -59,6 +55,19 @@
         >
         </v-list-item>
         <v-divider class="mt-2"></v-divider>
+      </template>
+      <template v-slot:item="{ props, item }">
+        <v-list-item
+          v-bind="props"
+          @click:append-icon="removeClicked(event, props)"
+        >
+          <template v-slot:append>
+            <v-icon
+              icon="fa-solid fa-xmark"
+              @click.stop="removeDomain(props.value)"
+            />
+          </template>
+        </v-list-item>
       </template>
     </v-select>
   </div>
