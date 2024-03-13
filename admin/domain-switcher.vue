ea939a7de17c6762d51ba984a501b9b6c6401ca1
Jason Ford <fordjason@gmail.com>
2024-03-12 21:31:00 -0700

    Update domain switcher

--- admin/domain-switcher.vue
+++ admin/domain-switcher.vue
@@ -6,7 +6,7 @@
 
   const domain = ref(router.currentRoute?.value?.params?.domain || null)
   const domainStates = ref(null)
-  const newDomainInput = ref('')
+  const domainToAdd = ref('')
 
   Agent.state('domains').then(state => {
     // add domain to viewed domains if not present
@@ -16,23 +16,23 @@
 
   const domains = computed(() => Object.keys(domainStates.value || {}))
 
-  function addDomain() {
-    const name = prompt('Enter domain name')
-    if (name) {
-      domainStates.value[name] = true
-      domain.value = name
-      manageDomain(name)
-    }
-  }
-
-  function manageDomain(domain) {
-    router.push(`/config/${domain}`)
-  }
-
   watch(domain, () => manageDomain(domain.value))
 
-  function removeDomain(domain) {
-    delete domainStates.value[domain]
+  function addDomain(name) {
+    domainStates.value[name] = true
+    domain.value = name
+    manageDomain(name)
+  }
+
+  function manageDomain(name) {
+    if (name) router.push(`/config/${name}`)
+    else router.push('/')
+  }
+
+  function removeDomain(name) {
+    console.log(domain.value, name)
+    if (domain.value === name) domain.value = null
+    delete domainStates.value[name]
   }
 
 </script>
@@ -43,32 +43,75 @@
     v-else
     class="d-flex"
   >
-    <v-select
-      v-model="domain"
-      :items="domains"
-      label="Selected Domain"
-    >
-      <template v-slot:prepend-item>
-        <v-list-item
-          title="Add New"
-          @click="addDomain"
-        >
-        </v-list-item>
-        <v-divider class="mt-2"></v-divider>
-      </template>
-      <template v-slot:item="{ props, item }">
-        <v-list-item
+    <v-menu label="Selected Domain">
+      <template v-slot:activator="{ props }">
+        <v-btn
+          color="primary"
+          dark
           v-bind="props"
-          @click:append-icon="removeClicked(event, props)"
         >
+          {{ domain || 'Select Domain' }}
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="name in domains"
+          :key="name"
+          @click="domain = name"
+        >
+          <v-list-item-title>{{ name }}</v-list-item-title>
           <template v-slot:append>
-            <v-icon
+            <v-btn
+              class="cursor-pointer"
+              variant="plain"
+              size="x-small"
               icon="fa-solid fa-xmark"
-              @click.stop="removeDomain(props.value)"
+              @click.stop="removeDomain(name)"
             />
           </template>
         </v-list-item>
-      </template>
-    </v-select>
+        <v-dialog max-width="500">
+          <template v-slot:activator="{ props: activatorProps }">
+            <v-divider class="mt-2"></v-divider>
+            <v-list-item
+              title="Add New"
+              v-bind="activatorProps"
+            />
+          </template>
+          <template v-slot:default="{ isActive }">
+            <v-card title="Add Domain">
+              <v-card-text>
+                <v-text-field
+                  autofocus
+                  v-model="domainToAdd"
+                  label="Domain"
+                  @keypress.enter="() => {
+                    addDomain(domainToAdd)
+                    domainToAdd = ''
+                    isActive.value = false
+                  }"
+                />
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn
+                  text="Add"
+                  @click="() => {
+                    addDomain(domainToAdd)
+                    domainToAdd = ''
+                    isActive.value = false
+                  }"
+                ></v-btn>
+                <v-btn
+                  text="Cancel"
+                  @click="isActive.value = false"
+                ></v-btn>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-dialog>
+      </v-list>
+    </v-menu>
   </div>
 </template>
