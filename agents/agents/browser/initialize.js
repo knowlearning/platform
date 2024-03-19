b545f6bb2cb95d151964ed0ef7d21e61e4ce430d
Jason Ford <fordjason@gmail.com>
2024-03-19 16:30:32 -0700

    Add file type accept parameter to browser upload

--- agents/agents/browser/initialize.js
+++ agents/agents/browser/initialize.js
@@ -18,8 +18,8 @@ export default function browserAgent(options={}) {
 
   const originalUpload = newAgent.upload
   newAgent.upload = async info => {
-    if (info && info.browser) {
-      const file = await selectFile()
+    if (info?.browser) {
+      const file = await selectFile(info)
       if (!file) return
 
       info.data = await file.arrayBuffer()
