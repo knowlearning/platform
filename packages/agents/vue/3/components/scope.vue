ddebdc34e8e8930b21b5d4a59b6a693751be7fad
Jason Ford <fordjason@gmail.com>
2024-04-13 19:01:34 -0700

    Update vueScopeComponent

--- packages/agents/vue/3/components/scope.vue
+++ packages/agents/vue/3/components/scope.vue
@@ -4,47 +4,46 @@
   </slot>
 </template>
 
-<script>
-import { ref, watch, onMounted, onBeforeUnmount, computed } from 'vue'
+<script setup>
+  import { ref, watch, onMounted, onBeforeUnmount, computed } from 'vue'
 
-export default {
-  props: {
+  const props = defineProps({
     id: String,
     path: { type: Array, default: [] },
     placeholder: { type: String, default: '' },
     metadata: { type: Boolean, default: false }
-  },
-  setup(props) {
-    const value = ref(undefined)
-    const loading = ref(true)
-    let stopWatching
-    let stopWatchingAttempted = false
+  })
 
-    const startWatching = async () => {
-      if (stopWatching) stopWatching()
-      if (props.metadata) {
-        const metadata = await Agent.metadata(props.id)
-        value.value = props.path.length === 1 ? metadata[props.path[0]] : metadata
-      } else {
-        stopWatching = Agent.watch([props.id, ...props.path], val => {
-          if (stopWatchingAttempted) console.warn('Watcher not stopped for vueScopeComponent')
-          else  value.value = val
-        })
-      }
-      loading.value = false
+  const value = ref(undefined)
+  const loading = ref(true)
+  let stopWatching
+
+  const startWatching = async () => {
+    if (stopWatching) stopWatching()
+
+    if (props.id === undefined) {
+      stopWatching = undefined
+      value.value = undefined
     }
-
-    watch( [() => props.id, () => props.path], startWatching, { deep: true })
-    onMounted(() => startWatching())
-    onBeforeUnmount(() => stopWatching && stopWatching())
-
-    const defaultRenderedValue = computed(() => {
-      if (loading.value) return 'loading'
-      else if (value.value === null && props.placeholder) return props.placeholder
-      else return value.value
-    })
-
-    return { loading, value, defaultRenderedValue }
+    else if (props.metadata) {
+      const metadata = await Agent.metadata(props.id)
+      value.value = props.path.length === 1 ? metadata[props.path[0]] : metadata
+    } else {
+      stopWatching = Agent.watch([props.id, ...props.path], v => value.value = v)
+    }
+    loading.value = false
   }
-};
+
+  watch( [() => props.id, () => props.path], startWatching, { deep: true })
+
+  onBeforeUnmount(() => stopWatching && stopWatching())
+
+  startWatching()
+
+  const defaultRenderedValue = computed(() => {
+    if (loading.value) return 'loading'
+    else if (value.value === null && props.placeholder) return props.placeholder
+    else return value.value
+  })
+
 </script>
