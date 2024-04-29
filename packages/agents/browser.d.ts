316e737b94d24f270ad45767ea92ab0078d40cc8
Stéphane Magnenat <stephane.magnenat@enlightware.ch>
2024-04-29 12:28:44 +0200

    Fixed Typescript compilation and updated browser.d.ts
    Two changes:
    - Added `declare` before `const agent: Agent`.
    - Updated `Agent` interface to match documentation.
    
    @jasonford one question: methods `environment`, `close`, `reset` and `synced` are not documented, are they typed correctly?

--- packages/agents/browser.d.ts
+++ packages/agents/browser.d.ts
@@ -17,15 +17,29 @@ export interface AgentEnvironment {
   context: string[];
 }
 
+export interface AgentUploadInfo {
+  name?: string,
+  type?: string,
+  data?: string | ArrayBuffer,
+  id: string,
+  browser?: boolean,
+  accept?: string,
+}
+
 export interface Agent {
+  login(provider: string): void;
+  logout(): void;
+  uuid(): string;
+  state(id: string, user?: string, domain?: string): Promise<object>;
+  metadata(id: string, user?: string, domain?: string): Promise<object>;
+  watch(id: string, callback: (update: { state: object }) => void, user?: string, domain?: string): void;
+  upload(info?: AgentUploadInfo): Promise<string>;
+  download(id?: string): Promise<Response>;
   environment(userId?: string): Promise<AgentEnvironment>;
-  state(ns?: string, user?: string, domain?: string): any;
-  upload(userId?: string): Promise<string>;
-  download(userId?: string): Promise<string>;
   close(): void;
   reset(ns: string): Promise<void>;
   synced(): Promise<void>;
 }
 
-const agent: Agent;
+declare const agent: Agent;
 export default agent;
