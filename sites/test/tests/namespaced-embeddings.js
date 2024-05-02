001e9aeeb5fd540f5cf81818071592e2dc1ad2c3
Jason Ford <fordjason@gmail.com>
2024-05-02 13:16:05 -0700

    Fix test

--- sites/test/tests/namespaced-embeddings.js
+++ sites/test/tests/namespaced-embeddings.js
@@ -83,7 +83,7 @@ export default function latestBugfixes() {
       await done
 
       expect(closeInfo.namespacedState).to.deep.equal(updatedNamespaceIncludedReference)
-      expect(closeInfo.unnamespacedState).to.deep.equal(updatedNamespaceIncludedReference)
+      expect(closeInfo.unnamespacedState).to.deep.equal(updatedNamespaceNotIncludedReference)
     })
   })
 }
