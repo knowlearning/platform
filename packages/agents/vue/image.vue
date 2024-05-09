ccc488b08b6280f49fdc54b799f35d792cbe4bee
Jason Ford <fordjason@gmail.com>
2024-05-09 14:09:04 -0700

    Add UUIDImage component for vue applications

---
+++ packages/agents/vue/image.vue
@@ -0,0 +1,31 @@
+<template>
+  <image
+    :key="downloadURL"
+    :src="downloadURL"
+  />
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import downloadURLRegistry from '../download-url-registry.js'
+
+  const props = defineProps({ id: String })
+
+  downloadURL = ref(downloadURLRegistry[props.id])
+
+  if (!downloadURL.value) setDownloadURL()
+
+  watch(props, setDownloadURL)
+
+  let latestRequest = 0
+  async function setDownloadURL() {
+    downloadURL.value = null
+    latestRequest += 1
+
+    const thisRequest = latestRequest
+    const newURL = downloadURLRegistry[props.id] || await Agent.download(props.id).url()
+
+    downloadURLRegistry[props.id] = newURL
+    if (latestRequest === thisRequest) downloadURL.value = newURL
+  }
+</script>
