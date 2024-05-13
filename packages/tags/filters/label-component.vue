4f278fc43796d4fa259839709134a0b59173c9ae
Jason Ford <fordjason@gmail.com>
2024-05-12 21:16:33 -0700

    Fix directive

--- packages/tags/filters/label-component.vue
+++ packages/tags/filters/label-component.vue
@@ -2,7 +2,7 @@
   <vueScopeComponent :id="props.id" :path="['name']" />
 </template>
 
-<script>
+<script setup>
   import { vueScopeComponent } from '@knowlearning/agents/vue.js'
 
   const props = defineProps({ id: String })
