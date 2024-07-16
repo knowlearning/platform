35154069ff1bd158b33eecee6450160733397330
Jason Ford <fordjason@gmail.com>
2024-07-16 12:43:54 -0700

    Start concating gcs files

--- core/source/monitor-streams.js
+++ core/source/monitor-streams.js
@@ -1,5 +1,6 @@
-import { createGCSClient, nats, environment, uuid } from './utils.js'
+import { createGCSClient, nats, environment, uuid, isUUID } from './utils.js'
 import { downloadURL } from './storage.js'
+import { equals } from "https://deno.land/std@0.224.0/bytes/mod.ts"
 
 const {
   INTERNAL_GCS_API_ENDPOINT,
@@ -21,6 +22,7 @@ const client = await nats.connect({ server: '0.0.0.0:4222' })
 const jsm = await client.jetstreamManager()
 const js = client.jetstream()
 const jc = nats.JSONCodec()
+const sc = nats.StringCodec()
 
 poll()
 
@@ -34,31 +36,63 @@ async function poll() {
         const messagesToSerialize = []
 
         const id = uuid()
-        const snapshot = await js.publish(config.name, jc.encode({ id }));
+        const snapshot = await js.publish(config.name, sc.encode(id))
+        let first = true
+        let idToConcat = null
 
         for await (const m of messages) {
+          if (first) {
+            first = false
+            if (bufferIsUUID(m.data)) idToConcat = new TextDecoder().decode(m.data)
+          }
+
           if (snapshot.seq > m.seq) {
-            messagesToSerialize.push(jc.decode(m.data))
+            messagesToSerialize.push(m.data)
             m.ack()
           }
           else break
         }
         try {
-          const data = messagesToSerialize.map(m => JSON.stringify(m)).join('\n')
-          console.log('UPLOADING', id)
-          await bucket.file(id).save(data)
-          console.log(
-            'UPLOADED TO', id,
-            await downloadURL(id)
-          )
+          //  TODO: investigate guarantees
+          const data = concatenateWithNewlines(messagesToSerialize)
+          if (idToConcat) {
+            const tmpId = uuid()
+            await bucket.file(tmpId).save(data)
+            const sources = [ bucket.file(idToConcat), bucket.file(tmpId) ]
+            await bucket.combine(sources, id)
+          }
+          else await bucket.file(id).save(data)
           await jsm.streams.purge(config.name, { seq: snapshot.seq })
-          console.log('PURGED THE OLD STUFF', id)
         }
-        catch (error) {
-          console.error('ERROR UPLOADING ROLLED UP STATE', error)
-        }
+        catch (error) { console.error('ERROR UPLOADING ROLLED UP STATE', error) }
       }
     })
   )
   setTimeout(poll, 5000)
+}
+
+// Create a newline Uint8Array
+const newline = new TextEncoder().encode('\n');
+
+// Function to concatenate buffers with newlines in between
+function concatenateWithNewlines(buffers) {
+  const totalLength = buffers.reduce((acc, buffer) => acc + buffer.length, 0) + (buffers.length - 1) * newline.length
+  const result = new Uint8Array(totalLength)
+
+  let offset = 0
+  buffers.forEach((buffer, index) => {
+    result.set(buffer, offset)
+    offset += buffer.length;
+    if (index < buffers.length - 1) {
+      result.set(newline, offset)
+      offset += newline.length
+    }
+  })
+
+  return result
+}
+
+function bufferIsUUID(uint8Array) {
+  const str = new TextDecoder().decode(uint8Array)
+  return isUUID(str)
 }
