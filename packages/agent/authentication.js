0a2cf65f6f044521fb8ee564f627080cf2296c17
Jason Ford <fordjason@gmail.com>
2024-07-22 16:35:00 -0700

    Simplify login script

--- packages/agent/authentication.js
+++ packages/agent/authentication.js
@@ -1,28 +1,33 @@
-
 const AUTH_HOST = 'https://auth.knowlearning.systems'
 
+const { location, localStorage } = window
+
 // auth token info is sent with pathnames of form /auth/VERIFICATION_STATE/PROVIDER_TOKEN
-if (window.location.pathname.startsWith('/auth/')) {
-  const state_token = window.location.pathname.slice(6)
+if (location.pathname.startsWith('/auth/')) {
+  const state_token = location.pathname.slice(6)
   const [state] = state_token.split('/', 1)
-  const origin = window.localStorage.getItem(state)
+  const origin = localStorage.getItem(state)
   if (origin) {
     const token = state_token.slice(state.length + 1)
-    window.localStorage.setItem('token', token)
-    window.location.href = origin
+    localStorage.setItem('token', token)
+    location.href = origin
   }
 }
 
+function randomToken() {
+  return Math.random().toString(36).substring(2)
+}
+
 function login(provider='google') {
-  const state = Math.random().toString(36).substring(2)
-  window.localStorage.setItem(state, window.location.href)
-  window.location.href = `${AUTH_HOST}/${provider}/${state}`
+  const state = randomToken()
+  localStorage.setItem(state, location.href)
+  location.href = `${AUTH_HOST}/${provider}/${state}`
 }
 
 function logout() {
   //  Doing this will establish a new anonymous session
-  window.localStorage.setItem('token', Math.random().toString(36).substring(2))
-  window.location.reload()
+  localStorage.setItem('token', randomToken())
+  location.reload()
 }
 
 export {
