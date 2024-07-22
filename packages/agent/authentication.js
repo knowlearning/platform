5809299ea46c52983a524f087125aa48f66ab50d
Jason Ford <fordjason@gmail.com>
2024-07-22 12:24:47 -0700

    Organize authentication logi

---
+++ packages/agent/authentication.js
@@ -0,0 +1,31 @@
+
+const AUTH_HOST = 'https://auth.knowlearning.systems'
+
+// auth token info is sent with pathnames of form /auth/VERIFICATION_STATE/PROVIDER_TOKEN
+if (window.location.pathname.startsWith('/auth/')) {
+  const state_token = window.location.pathname.slice(6)
+  const [state] = state_token.split('/', 1)
+  const origin = window.localStorage.getItem(state)
+  if (origin) {
+    const token = state_token.slice(state.length + 1)
+    window.localStorage.setItem('token', token)
+    window.location.href = origin
+  }
+}
+
+function login(provider='google') {
+  const state = Math.random().toString(36).substring(2)
+  window.localStorage.setItem(state, window.location.href)
+  window.location.href = `${AUTH_HOST}/${provider}/${state}`
+}
+
+function logout() {
+  //  Doing this will establish a new anonymous session
+  window.localStorage.setItem('token', Math.random().toString(36).substring(2))
+  window.location.reload()
+}
+
+export {
+  login,
+  logout
+}
