a2912327a81127a37f4a0f90f466f2cdcc8fc999
Jason Ford <fordjason@gmail.com>
2024-07-23 11:10:52 -0700

    Add nats client to agent

--- packages/agent/index.js
+++ packages/agent/index.js
@@ -1,6 +1,31 @@
 import { login, logout } from './authentication.js'
+import { connect, JSONCodec } from 'nats.ws'
+
+const decodeJSON = JSONCodec()
+const natsClientPromise = connect({ servers: ['ws://localhost:8080'] })
 
 export default {
   login,
-  logout
+  logout,
+  state,
+  watch
+}
+
+async function watch(scope, callback, user='me', domain=window.location.host) {
+  const client = await natsClientPromise
+
+  const subject = `${domain}.${user}.${scope}`
+  const subscription = client.subscribe(subject)
+
+  ;(async () => {
+    for await (const message of subscription) {
+      callback(decodeJSON(message.data))
+    }
+  })()
+
+  //  TODO: return unsubscribe function
+}
+
+async function state(scope, user, domain) {
+  
 }
