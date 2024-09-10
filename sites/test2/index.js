1e256aa73f0d33152d58a099167646bc64fdaac7
Jason Ford <fordjason@gmail.com>
2024-09-09 17:03:58 -0700

    Add more embedded tests

--- sites/test2/index.js
+++ sites/test2/index.js
@@ -18,12 +18,76 @@ import postgresTest from '../test/tests/postgres.js'
 
 window.Agent = Agent
 
+const id = window.location.pathname.slice(1)
 const { mode } = await Agent.environment()
 
 if (mode === 'EMBEDED_QUERY_TEST_MODE') {
   const result = await Agent.query('my-test-table-entries')
   Agent.close(result)
 }
+else if (mode === 'EMBEDED_WATCHER_TEST_MODE') {
+  const states = []
+  const unwatch = Agent.watch(id, update => {
+    const { state } = update
+    console.log('UPDATE!!!!!!!!!!!!', update)
+    states.push(state)
+    if (state.done) {
+      Agent.close(JSON.parse(JSON.stringify(states)))
+      unwatch()
+    }
+  })
+}
+/* TODO: enable the tests for these modes!
+else if (mode === 'EMBEDED_QUERY_ERROR_TEST_MODE') {
+  try {
+    const response = await Agent.query('no-query-named-this')
+    Agent.close('NO ERROR THROWN... response was ' + response)
+  }
+  catch (error) {
+    Agent.close(null)
+  }
+}
+else if (mode === 'EMBEDED_PARALLEL_QUERY_TEST_MODE') {
+  const numParallelQueries = 1000
+  const queries = []
+  for (let i=0; i<numParallelQueries; i++) {
+    queries.push(Agent.query('my-test-table-entries'))
+  }
+  const results = await Promise.all(queries)
+  Agent.close(null)
+}
+else if (mode === 'EMBEDED_SCOPE_NAMESPACE_TEST_MODE') {
+  const scope ='some-namespaced-scope-name'
+  Agent.watch(scope, async ({ state }) => {
+    if (state.modified && state.modifiedInEmbed) Agent.close(JSON.parse(JSON.stringify(state)))
+  })
+  const s = await Agent.state(scope)
+  s.modifiedInEmbed = true
+}
+else if (mode === 'EMBEDED_SCOPE_NAMESPACE_ALLOW_TEST_MODE') {
+  const scope ='some-namespaced-scope-name'
+  const unnamespacedScope = 'this-avoids-namespacing/' + scope
+  Agent.watch(unnamespacedScope, async ({ state: unnamespacedState }) => {
+
+    if (unnamespacedState.modifiedInEmbed) {
+      Agent.watch(scope, async ({ state: namespacedState }) => {
+      //  TODO: FIX: the namespace is "[object, Object]" since we're allowing a richer namespace object with an allow list
+        if (namespacedState.modified && namespacedState.modifiedInEmbed) Agent.close(JSON.parse(JSON.stringify({
+          unnamespacedState,
+          namespacedState
+        })))
+      })
+      const s = await Agent.state(scope)
+      s.modifiedInEmbed = true
+    }
+  })
+  const s = await Agent.state(unnamespacedScope)
+  s.modifiedInEmbed = true
+}
+else if (mode === 'EMBEDDED_ENVIRONMENT_TEST_MODE') {
+  const id = uuid()
+  Agent.close(await Agent.environment(id))
+}*/
 else {
   window.expect = chai.expect
   window.uuid = uuid
@@ -50,7 +114,6 @@ else {
     vuexTest(vuePersistentStore)
     uploadTest()
     latestBugfixesTest()
-    console.log('IS AGENT EMBEDDED???', Agent.embedded)
     if (!Agent.embedded) postgresTest()
   })
 
