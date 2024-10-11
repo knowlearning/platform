87a726702c8b09f314b0442a45c7b403792527c9
Jason Ford <fordjason@gmail.com>
2024-10-11 15:48:46 -0700

    Start adding remote core worker run script

--- core2/infrastructure/GCP/core-workers.js
+++ core2/infrastructure/GCP/core-workers.js
@@ -16,30 +16,10 @@ export default function ({ NATS_IP_ADDRESS, REDIS_IP_ADDRESS, region, machineTyp
         }],
         metadataStartupScript: `
             #! /bin/sh
-            fetch_secret() {
-                local secret_name=$1
-                local secret_value=$(gcloud secrets versions access latest --secret="$secret_name" 2>/dev/null)
-                if [ $? -eq 0 ]; then
-                    echo "$secret_value"
-                else
-                    echo "Failed to retrieve secret: $secret_name" >&2
-                    exit 1
-                fi
-            }
-
-            echo "Fetching secret from Google Cloud Secret Manager..."
-            SECRET=$(fetch_secret "test-secret")
-            echo "Got secret"
-            echo $SECRET
-            echo "next thing to do is to connect to nats on ${NATS_IP_ADDRESS}"
-            sudo apt-get install redis-tools
-            REDIS_STATUS=$(redis-cli -h ${REDIS_IP_ADDRESS} -p 6379 ping)
-
-            if [ "$REDIS_STATUS" == "PONG" ]; then
-                echo "Redis server is UP and reachable!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-            else
-                echo "Redis server is NOT reachable!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!:("
-            fi
+            curl -fsSL https://deno.land/install.sh | sh
+            sudo apt install git
+            git clone https://github.com/knowlearning/platform.git
+            cd platform/run.sh
         `,
         serviceAccount: {
             scopes: ["https://www.googleapis.com/auth/cloud-platform"]
