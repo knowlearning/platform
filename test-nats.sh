a095310e52b142c313751b728e2978b93d8cfaa5
Jason Ford <fordjason@gmail.com>
2024-10-15 15:31:00 -0700

    Update NATS cluster ip

--- test-nats.sh
+++ test-nats.sh
@@ -1,2 +1,6 @@
-nats sub -s "nats://34.16.126.61:4222" hello &
-nats pub -s "nats://34.16.126.61:4222" hello world_4222
+curl -sf https://binaries.nats.dev/nats-io/natscli/nats | sh
+
+nats server check connection -s nats://35.192.110.199:4222
+
+nats sub -s "nats://35.192.110.199:4222" hello &
+nats pub -s "nats://35.192.110.199:4222" hello world_4222
