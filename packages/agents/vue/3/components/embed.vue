a7e099e86531a53592e6ee1825147cc65612a0f6
Jason Ford <fordjason@gmail.com>
2024-12-13 19:26:50 -0800

    Fix reference

--- packages/agents/vue/3/components/embed.vue
+++ packages/agents/vue/3/components/embed.vue
@@ -57,7 +57,7 @@ export default {
     startWatching() {
       if (this.stopWatching) this.stopWatching()
       if (this.path.length) {
-        this.stopWatching = Agent().watch([this.id, ...this.path], value => {
+        this.stopWatching = Agent.watch([this.id, ...this.path], value => {
           //  TODO: ensure resolved value is uuid or URL
           this.resolvedId = value
         })
@@ -68,8 +68,8 @@ export default {
       if (!iframe || this.iframe === iframe) return
 
       this.iframe = iframe
-      this.embedding = Agent().embed({ id, mode, namespace }, iframe)
-      this.embedding.on('environment', e => this.environmentProxy ? this.environmentProxy(e) : Agent().environment(e))
+      this.embedding = Agent.embed({ id, mode, namespace }, iframe)
+      this.embedding.on('environment', e => this.environmentProxy ? this.environmentProxy(e) : Agent.environment(e))
       this.embedding.on('state', e => this.$emit('state', e))
       this.embedding.on('mutate', e => this.$emit('mutate', e))
       this.embedding.on('close', e => this.$emit('close', e))
