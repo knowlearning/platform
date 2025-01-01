98827e0e64ea4c193353b25dcc209559181f535f
Jason Ford <fordjason@gmail.com>
2024-12-31 17:41:04 -0800

    Decompose deployment script

--- core/infrastructure/GCP/infrastructure-request.js
+++ core/infrastructure/GCP/infrastructure-request.js
@@ -1,37 +1,11 @@
-let token
-
-async function getGCPAccessToken() {
-  if (token) return token
-
-  let resolve, reject
-  token = new Promise((res, rej) => {
-  	resolve = res
-  	reject = rej
-  })
-
-  const cmd = Deno.run({
-    cmd: ["gcloud", "auth", "application-default", "print-access-token"],
-    stdout: "piped",
-    stderr: "piped"
-  })
-
-  const output = await cmd.output()
-  const error = await cmd.stderrOutput()
-
-  cmd.close()
-
-  if (error.length > 0) reject(`Error fetching access token: ${new TextDecoder().decode(error)}`)
-  else resolve(new TextDecoder().decode(output).trim())
-
-  return token
-}
+import getAccessToken from './get-access-token.js'
 
 export default async function infrastructureRequest(provider, url, body) {
   if (provider === 'GCP') {
     return await fetch(url, {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${await getGCPAccessToken()}`,
+        "Authorization": `Bearer ${await getAccessToken(provider)}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify(body)
