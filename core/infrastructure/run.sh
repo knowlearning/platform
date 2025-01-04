0691d61011da0dc8891e5d4e3366875092fb50e7
Jason Ford <fordjason@gmail.com>
2025-01-03 20:36:59 -0800

    Move run file

--- core/infrastructure/run.sh
+++ core/infrastructure/run.sh
@@ -1,15 +1,11 @@
-sudo apt install unzip
+sudo apt install git unzip -y
 curl -fsSL https://deno.land/install.sh | sh -s -- -y
 
-echo "SETTING ENVIRONMENT VARIABLES"
+git clone https://github.com/knowlearning/platform.git
 
-AUTH_SERVICE_SECRET_KEY=$(gcloud secrets versions access latest --secret=AUTH_SERVICE_SECRET_KEY)
-GCS_SERVICE_ACCOUNT_CREDENTIALS=$(gcloud secrets versions access latest --secret=GCS_SERVICE_ACCOUNT_CREDENTIALS)
-OAUTH_CREDENTIALS=$(gcloud secrets versions access latest --secret=OAUTH_CREDENTIALS)
-POSTGRES_PASSWORD=$(gcloud secrets versions access latest --secret=POSTGRES_PASSWORD)
-REDIS_PASSWORD=$(gcloud secrets versions access latest --secret=REDIS_PASSWORD)
-INSECURE_DEVELOPMENT_CERT=$(gcloud secrets versions access latest --secret=INSECURE_DEVELOPMENT_CERT)
-INSECURE_DEVELOPMENT_KEY=$(gcloud secrets versions access latest --secret=INSECURE_DEVELOPMENT_KEY)
+git checkout -b update-infrastructure
+
+echo "SETTING ENVIRONMENT VARIABLES"
 GCS_BUCKET_NAME=development-bucket-opensourcelearningplatform
 MODE=production
 ADMIN_DOMAIN=admin.knowlearning.systems
@@ -24,7 +20,6 @@ PORT=80
 TLS_PORT=443
 
 echo "STARTING SERVER"
-
 AUTH_SERVICE_SECRET_KEY="$AUTH_SERVICE_SECRET_KEY" \
 GCS_SERVICE_ACCOUNT_CREDENTIALS="$GCS_SERVICE_ACCOUNT_CREDENTIALS" \
 OAUTH_CREDENTIALS="$OAUTH_CREDENTIALS" \
@@ -44,7 +39,7 @@ PORT="$PORT" \
 TLS_PORT="$TLS_PORT" \
 INSECURE_DEVELOPMENT_CERT="$INSECURE_DEVELOPMENT_CERT" \
 INSECURE_DEVELOPMENT_KEY="$INSECURE_DEVELOPMENT_KEY" \
-/root/.deno/bin/deno run \
+~/.deno/bin/deno run \
   --allow-sys \
   --allow-net \
   --allow-write \
@@ -52,4 +47,4 @@ INSECURE_DEVELOPMENT_KEY="$INSECURE_DEVELOPMENT_KEY" \
   --unstable-worker-options \
   --v8-flags=--max-old-space-size=8000 \
   --allow-env \
-  ./source/index.js
+  ./platform/core/source/index.js
