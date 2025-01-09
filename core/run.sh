1c10320958d1bff5bf1359a81eb48844279d6d29
Jason Ford <fordjason@gmail.com>
2025-01-08 20:32:47 -0800

    Revert "Set deno env var for tls certs"
    This reverts commit 905abbbcf9f2620c3e3621c92cb61f28bcb7c44c.

--- core/run.sh
+++ core/run.sh
@@ -11,6 +11,7 @@ while true; do
         --allow-write \
         --allow-read \
         --unstable-worker-options \
+        --unsafely-ignore-certificate-errors \
         --v8-flags=--max-old-space-size=8000 \
         --allow-env \
         $SCRIPT_DIR/source/index.js
