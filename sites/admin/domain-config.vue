6dc9a91f08361a6b2ba7453cba72c4f6574f2113
Jason Ford <fordjason@gmail.com>
2025-02-24 14:20:11 -0800

    Use setup function for domain config page

--- sites/admin/domain-config.vue
+++ sites/admin/domain-config.vue
@@ -33,8 +33,8 @@
   </div>
 </template>
 
-<script>
-
+<script setup>
+import { ref } from 'vue'
 import { vueScopeComponent } from '@knowlearning/agents/vue.js'
 import ReportViewer from './report-viewer.vue'
 import YAMLEditor from './codemirror/yaml-editor.vue'
@@ -42,90 +42,66 @@ import YAMLValueReplacer from './yaml-value-replacer.vue'
 
 const DOMAIN_CONFIG_TYPE = 'application/json;type=domain-config'
 
-export default {
-  props: {
-    domain: String
-  },
-  components: {
-    vueScopeComponent,
-    ReportViewer,
-    YAMLEditor,
-    YAMLValueReplacer
-  },
-  data() {
+const { domain } = defineProps({ domain: String })
+
+const { auth: { user, provider } } = await Agent.environment()
+const config = await Agent.state(domain, window.location.host)
+const claimMessage = ref(null)
+const claimReport = ref(null)
+
+async function claim() {
+  const start = Date.now()
+  claimMessage.value = 'claiming...'
+  const { token, report } = await Agent.claim(this.domain)
+  claimReport.value = report
+  const elapsed = Date.now() - start
+  await new Promise(r => setTimeout(r, 500 - elapsed))
+
+  if (domain.startsWith(`${user}.localhost:`)) {
+    claimMessage.value = `You are now registered as the admin of ${domain}.`
+  }
+  else {
+    claimMessage.value = `
+      Set "${token}" as a TXT record for "${domain}" become the admin.
+      Alternatively, make your website "${domain}/.well-known/knowlearning-admin-challenge" respond with "${token}"
+    `
+  }
+}
+
+
+async function uploadConfig() {
+  const id = await Agent.upload({ browser: true, accept: '.yml,.yaml' })
+
+  const report = Agent.uuid()
+
+  await Agent.create({
+    active: { config: id, report, domain },
+    active_type: DOMAIN_CONFIG_TYPE
+  })
+
+  await Agent.synced()
+
+  this.config = (await Agent.query('current-config', [domain]))[0]
+}
+
+function downloadConfig(id) {
+  Agent.download(id).direct()
+}
+
+function resolveWidget(path) {
+  if (path[0] === 'widget') {
     return {
-      user: null,
-      provider: null,
-      config: null,
-      claimReport: null,
-      claimMessage: null
-    }
-  },
-  async created() {
-    const { auth: { user, provider } } = await Agent.environment()
-
-    this.user = user
-    this.provider = provider
-
-    this.config = (await Agent.query('current-config', [this.domain]))[0]
-  },
-  methods: {
-    async claim() {
-      const start = Date.now()
-      this.claimMessage = 'claiming...'
-      const { token, report } = await Agent.claim(this.domain)
-      this.claimReport = report
-      const elapsed = Date.now() - start
-      await new Promise(r => setTimeout(r, 500 - elapsed))
-
-
-      if (this.domain.startsWith(`${this.user}.localhost:`)) {
-        this.claimMessage = `You are now registered as the admin of ${this.domain}.`
-      }
-      else {
-        this.claimMessage = `
-          Set "${token}" as a TXT record for "${this.domain}" become the admin.
-          Alternatively, make your website "${this.domain}/.well-known/knowlearning-admin-challenge" respond with "${token}"
-        `
-      }
-    },
-    async removeDomainConfig() {
-      if (confirm(`Are you sure you want to remove your configuration for "${this.domain}"`)) {
-        delete this.config[this.domain]
-      }
-    },
-    async uploadConfig() {
-      const id = await Agent.upload({ browser: true, accept: '.yml,.yaml' })
-
-      const report = Agent.uuid()
-
-      await Agent.create({
-        active: { config: id, report, domain: this.domain },
-        active_type: DOMAIN_CONFIG_TYPE
-      })
-
-      await Agent.synced()
-
-      this.config = (await Agent.query('current-config', [this.domain]))[0]
-    },
-    downloadConfig(id) {
-      Agent.download(id).direct()
-    },
-    resolveWidget(path) {
-      if (path[0] === 'widget') {
-        return {
-          component: YAMLValueReplacer,
-          props: {}
-        }
-      }
-      else return null
-    },
-    arrayMatch(a, b) {
-      return a.every((v, i) => {
-        return v === '*' || b[i] === '*' || v === b[i]
-      })
+      component: YAMLValueReplacer,
+      props: {}
     }
   }
+  else return null
+}
+
+function arrayMatch(a, b) {
+  return a.every((v, i) => {
+    return v === '*' || b[i] === '*' || v === b[i]
+  })
 }
 
 </script>
--- sites/admin/home.vue
+++ sites/admin/home.vue
@@ -1,53 +1,55 @@
 <template>
-  <div v-if="!auth?.provider">loading...</div>
-  <div v-else-if="auth?.provider === 'anonymous'">
-    <v-btn
-      prepend-icon="fa-solid fa-right-to-bracket"
-      @click="login"
-    >
-      login
-    </v-btn>
-  </div>
-  <div v-else>
-    <v-toolbar
-      color="primary"
-    >
-      <DomainSwitcher
-        class="ms-2"
-        @select="name => {
-          if (name) $router.push(`/${name}/${tab || 'config'}`)
-          else $router.push('/')
-        }"
-      />
-      <v-toolbar-title>
-        {{ domain }}
-      </v-toolbar-title>
-      <v-tabs
-        v-model="selectedTab"
-        bg-color="primary"
-        v-if="domain"
-      >
-        <v-tab value="config">Configuration</v-tab>
-        <v-tab value="agents">Agents</v-tab>
-        <v-tab value="sql">SQL</v-tab>
-        <v-tab value="tests">Tests</v-tab>
-      </v-tabs>
-      <v-spacer />
+  <Suspense>
+    <div v-if="!auth?.provider">loading...</div>
+    <div v-else-if="auth?.provider === 'anonymous'">
       <v-btn
-        @click="logout"
-        append-icon="fa-solid fa-arrow-right-from-bracket"
+        prepend-icon="fa-solid fa-right-to-bracket"
+        @click="login"
       >
-        Logout
+        login
       </v-btn>
-      <v-avatar
-        class="ms-4 me-4"
-        :image="auth.info.picture"
-      />
-    </v-toolbar>
-    <v-container :key="domain">
-      <router-view></router-view>
-    </v-container>
-  </div>
+    </div>
+    <div v-else>
+      <v-toolbar
+        color="primary"
+      >
+        <DomainSwitcher
+          class="ms-2"
+          @select="name => {
+            if (name) $router.push(`/${name}/${tab || 'config'}`)
+            else $router.push('/')
+          }"
+        />
+        <v-toolbar-title>
+          {{ domain }}
+        </v-toolbar-title>
+        <v-tabs
+          v-model="selectedTab"
+          bg-color="primary"
+          v-if="domain"
+        >
+          <v-tab value="config">Configuration</v-tab>
+          <v-tab value="agents">Agents</v-tab>
+          <v-tab value="sql">SQL</v-tab>
+          <v-tab value="tests">Tests</v-tab>
+        </v-tabs>
+        <v-spacer />
+        <v-btn
+          @click="logout"
+          append-icon="fa-solid fa-arrow-right-from-bracket"
+        >
+          Logout
+        </v-btn>
+        <v-avatar
+          class="ms-4 me-4"
+          :image="auth.info.picture"
+        />
+      </v-toolbar>
+      <v-container :key="domain">
+        <router-view></router-view>
+      </v-container>
+    </div>
+  </Suspense>
 </template>
 
 <script>
