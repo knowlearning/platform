96a1983afbe712f0f37247fb3f06aaed3bcbf2c7
Jason Ford <fordjason@gmail.com>
2025-05-03 16:39:29 -0700

    Improved code auth (#38)
    * Update backend code checking
    
    * Fix imports

---
+++ core/source/authenticate/decrypt-json.js
@@ -0,0 +1,23 @@
+import { decodeBase64, box } from '../utils.js'
+
+export default function decryptJSON(secretKey, publicKey, encryptedMessage) {
+  return JSON.parse(
+    new TextDecoder().decode(
+      decrypt(
+        decodeBase64(secretKey),
+        decodeBase64(publicKey),
+        decodeBase64(encryptedMessage)
+      )
+    )
+  )
+}
+
+const decrypt = (mySecretKey, theirPublicKey, encryptedMessageBufferWithNonce) => {
+  const nonce = encryptedMessageBufferWithNonce.slice(0, box.nonceLength)
+  const encryptedMessageBuffer = encryptedMessageBufferWithNonce.slice(box.nonceLength)
+  const decrypted = box.open(encryptedMessageBuffer, nonce, theirPublicKey, mySecretKey)
+
+  if (!decrypted) throw new Error('Could not decrypt message')
+
+  return decrypted
+}
