5ef3df29ce65bb8ef310c05749c1082dde88f30f
Jason Ford <fordjason@gmail.com>
2025-06-03 17:48:20 -0700

    Add us southeast region

--- core/infrastructure/GCP/publish.sh
+++ core/infrastructure/GCP/publish.sh
@@ -34,9 +34,14 @@ if [ "$1" = "production" ]; then
   export ZONE="us-central1-a"
   export INSTANCE_NAME="my-deno-instance-1"
   export STATIC_IP_NAME="my-static-ip-1"
+  # export REGION="us-central1"
   # export ZONE="us-central1-b"
   # export INSTANCE_NAME="my-deno-instance-2"
   # export STATIC_IP_NAME="my-static-ip-2"
+  # export REGION="asia-southeast1"
+  # export ZONE="asia-southeast1-a"
+  # export INSTANCE_NAME="my-deno-instance-3"
+  # export STATIC_IP_NAME="my-static-ip-3"
 
   export GCP_MACHINE="n4-standard-2"
 
