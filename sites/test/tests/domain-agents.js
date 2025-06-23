0f95321fa634dc8923d0748f83ba190a1f139e0c
Jason Ford <fordjason@gmail.com>
2025-06-23 14:46:30 -0700

    Remove old test logs

--- sites/test/tests/domain-agents.js
+++ sites/test/tests/domain-agents.js
@@ -185,13 +185,11 @@ agent: |
       })
 
     child.on('mutate', async mutation => {
-      console.log('MUTATION!!!!!!!!!!!!!!!!!!!', mutation)
       if (mutation.scope.startsWith('mirror-no-reset')) {
         const myState = await Agent.state(mutation.scope)
         fastJSONPatch.applyPatch(myState, standardJSONPatch(mutation.patch))
       }
       else if (mutation.scope.startsWith('mirror-reconnect')) {
-        console.log('AGENT RECONNECTING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         Agent.reconnect()
         const myState = await Agent.state(mutation.scope)
         setTimeout(() => {
