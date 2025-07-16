b8aa16c0e5886fa47996fc71af7787533f7cebae
Jason Ford <fordjason@gmail.com>
2025-07-16 14:49:29 -0700

    Play nicer with supervisord

--- core/run.sh
+++ core/run.sh
@@ -2,20 +2,18 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Keep the server running and auto-restart on exit
-while true; do
-    echo "$(date): Starting service..." >> $1
-    sudo -E ~/.deno/bin/deno run \
-        --allow-sys \
-        --allow-net \
-        --allow-write \
-        --allow-read \
-        --allow-run \
-        --cert=/etc/ssl/certs/ca-certificates.crt \
-        --v8-flags=--max-old-space-size=8000 \
-        --allow-env \
-        $SCRIPT_DIR/source/index.js
-    EXIT_STATUS=$?
-    echo "$(date): Service exited with status $EXIT_STATUS. Restarting in 2 seconds..." >> "$1"
-    sleep 2
-done
+sudo -E ~/.deno/bin/deno run \
+    --allow-sys \
+    --allow-net \
+    --allow-write \
+    --allow-read \
+    --allow-run \
+    --cert=/etc/ssl/certs/ca-certificates.crt \
+    --v8-flags=--max-old-space-size=8000 \
+    --allow-env \
+    $SCRIPT_DIR/source/index.js 2>&1 | while IFS= read -r line; do
+      echo "[$(date '+%Y-%m-%d %H:%M:%S')] $line"
+    done &
+child=$!
+
+wait "$child"
