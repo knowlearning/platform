8b28cfc19725a0b80b3bf9e149eb1a49a8c2273b
Jason Ford <fordjason@gmail.com>
2025-09-22 15:31:01 -0700

    Follow working pattern

--- sites/admin/domain-sql.vue
+++ sites/admin/domain-sql.vue
@@ -27,7 +27,7 @@
 
 <script setup>
 import { ref, computed } from 'vue'
-import converter from 'json-2-csv'
+import { json2csv } from 'json-2-csv'
 
 // Props
 const props = defineProps({
@@ -52,7 +52,7 @@ async function downloadCSV() {
   if (!response.value || !response.value.length) return
 
   try {
-    const csv = await converter.json2csvAsync(response.value)
+    const csv = await json2csv(response.value)
     const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' })
     const url = URL.createObjectURL(blob)
 
