5c57d372183894ed9bbeddf0b30ad879acfa035d
Jason Ford <fordjason@gmail.com>
2025-09-22 15:03:16 -0700

    Add download button to admin sql page

--- sites/admin/domain-sql.vue
+++ sites/admin/domain-sql.vue
@@ -9,49 +9,64 @@
       />
     </v-card-text>
     <v-card-actions right>
-      <v-btn
-        text
-        @click="submitQuery"
-      >
+      <v-btn text @click="submitQuery">
         Submit
       </v-btn>
     </v-card-actions>
   </v-card>
-  <v-card
-    v-if="response !== null"
-    class="mt-8"
-  >
+
+  <v-card v-if="response !== null" class="mt-8">
     <v-container>
+      <v-btn text @click="downloadCSV">
+        Download CSV
+      </v-btn>
       <v-data-table sticky :items="response"></v-data-table>
     </v-container>
   </v-card>
 </template>
 
-<script>
+<script setup>
+import { ref, computed } from 'vue'
+import { json2csvAsync } from 'json-2-csv'
 
-  export default {
-    props: {
-      domain: String
-    },
-    data() {
-      return {
-        query: 'SELECT * FROM metadata LIMIT 20',
-        response: null
-      }
-    },
-    methods: {
-      async submitQuery() {
-        this.response = await Agent.query(this.query, [], this.domain)
-        console.log('RESPONSE!!!!!!!!', this.response)
-      }
-    },
-    computed: {
-      columns() {
-        return this.response && this.response.length ? Object.keys(this.response[0]) : []
-      }
-    }
+// Props
+const props = defineProps({
+  domain: String
+})
+
+// State
+const query = ref('SELECT * FROM metadata LIMIT 20')
+const response = ref(null)
+
+// Methods
+async function submitQuery() {
+  response.value = await Agent.query(query.value, [], props.domain)
+  console.log('RESPONSE!!!!!!!!', response.value)
+}
+
+const columns = computed(() =>
+  response.value && response.value.length ? Object.keys(response.value[0]) : []
+)
+
+async function downloadCSV() {
+  if (!response.value || !response.value.length) return
+
+  try {
+    const csv = await json2csvAsync(response.value)
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' })
+    const url = URL.createObjectURL(blob)
+
+    const link = document.createElement('a')
+    link.href = url
+    link.setAttribute('download', 'query_results.csv')
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    URL.revokeObjectURL(url)
+  } catch (err) {
+    console.error('Error converting to CSV:', err)
   }
-
+}
 </script>
 
 <style scoped>
