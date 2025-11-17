b74aba23d5a98464fa4fe0ce2f77a1f4382333d8
Jason Ford <fordjason@gmail.com>
2025-11-17 14:48:41 -0800

    remove hack for now

--- sites/embed/player.vue
+++ sites/embed/player.vue
@@ -121,7 +121,6 @@
         await Agent.synced()
         try { await Agent.response() }
         catch (error) {}
-        await new Promise(r => setTimeout(r, 1000))
         const latestCompetencies = await Agent.state(`pila/latest_competencies/${candliGameId}`)
         Agent.close({ competencies: copy(latestCompetencies), success: info?.success })
       }
