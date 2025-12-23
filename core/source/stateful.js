a8da73b61417437362434ea552348cb176d465f2
Jason Ford <fordjason@gmail.com>
2025-12-22 20:44:14 -0800

    Start commenting

--- core/source/stateful.js
+++ core/source/stateful.js
@@ -1,13 +1,13 @@
-export const DomainAgents = {}
-export const configuredDomains = {}
-export const agents = {}
-export const configCache = {}
-export const schemaCache = {}
-export const outstandingCoreStateInteractions = new Set()
-export const domainWorkers = {}
-export const domainWorkerResponses = {}
-export const gcpTokenCache = new Map()
-export const guarantees = {}
+export const DomainAgents = {}  // not needed to sync, created on demand (deprecating anyway)
+export const configuredDomains = {} // not needed to sync, caches on demand
+export const agents = {} // no need to sync, will re-create on demand
+export const configCache = {} // X caches on demand
+export const schemaCache = {} // X consider removing entirely if schema's no longer in use
+export const outstandingCoreStateInteractions = new Set() // X okay to ignore as this tracks responses that will not come back
+export const domainWorkers = {} // X not needed as they will be cached
+export const domainWorkerResponses = {} // O might be needed as error responses need to be sent (are these accounted for already with patch responses?)
+export const gcpTokenCache = new Map() // X will be cached again
+export const guarantees = {} // O needs to be held across closures
 
 export const activeConnections = {}
 export const sessionMessageIndexes = {}
