9422c8d798513dc62f77abbebc0d205a0ba8e72c
Jason Ford <fordjason@gmail.com>
2026-01-14 23:52:12 -0800

    Add function to get and cache domain connections by db id

--- core/source/redis.js
+++ core/source/redis.js
@@ -3,61 +3,79 @@ import { createRedisClient, environment } from './utils.js'
 const {
   REDIS_HOST,
   REDIS_PORT,
-  REDIS_PASSWORD,
-  REDIS_DATABASE_CREDENTIALS
+  REDIS_PASSWORD
 } = environment
+
+//  TODO: store in JSON in env
+
+
+const REDIS_DATABASE_CREDENTIALS = {
+  'thailand-ap-southeast-1': { HOST: REDIS_HOST, PORT: REDIS_PORT, PASSWORD: REDIS_PASSWORD },
+  'default-us-east-1': { HOST: REDIS_HOST, PORT: REDIS_PORT, PASSWORD: REDIS_PASSWORD },
+}
 
 const scopeDomainCache = {
   'domain-config': 'core' // TODO relate the value to a client were that scope is held
 }
 
-const domainToDatabaseConfig = {
-  'thailand.pilaproject.org': 'ap-southeast-1',
-  default: 'us-east-1'
+const domainToDatabaseId = {
+  'thailand.pilaproject.org': 'thailand-ap-southeast-1',
+  default: 'default-us-east-1'
 }
 
 
 
 
 ///////////////////////////////////////////////////////////////
-//  TODO: create a client/subscription for every entry in
-//        REDIS_DATABASE_CREDENTIALS
+//  TODO: getConnection for every entry in
+//        REDIS_DATABASE_CREDENTIALS and store in
+//        databaseIdToConnection cache
 ///////////////////////////////////////////////////////////////
 
-const clientConnectionInfo = {
-  socket: {
-    host: REDIS_HOST,
-    port: REDIS_PORT
-  },
-  password: REDIS_PASSWORD
+
+
+
+
+
+
+
+
+const clientCache = {}
+
+async function getConnection(databaseId) {
+  if (clientCache[databaseId]) return clientCache[databaseId]
+
+  const { HOST, PORT, PASSWORD } = REDIS_DATABASE_CREDENTIALS[databaseId]
+  const connectionInfo = {
+    socket: { host: HOST, port: PORT },
+    password: PASSWORD
+  }
+  const client = createRedisClient(connectionInfo)
+  const subscriptions = createRedisClient(connectionInfo)
+
+  client.on('error', e => console.warn('ERROR CONNECTING TO REDIS', e.toString()))
+  subscriptions.on('error', e => console.warn('ERROR CONNECTING TO REDIS', e.toString()))
+
+  clientCache[databaseId] = (
+    Promise
+      .all([ client.connect(), subscriptions.connect() ])
+      .then(() => ({ client, subscriptions }))
+  )
+
+  return clientCache[databaseId]
 }
 
-const client = createRedisClient(clientConnectionInfo)
-const subscriptions = createRedisClient(clientConnectionInfo)
-
-client.on('error', e => console.warn('ERROR CONNECTING TO REDIS', e.toString()))
-subscriptions.on('error', e => console.warn('ERROR CONNECTING TO REDIS', e.toString()))
-
-const clientConnection = client.connect()
-const subscriptionsConnection = subscriptions.connect()
-
-
-
-
-
-
+//  TODO: REPLACE ALL getConnection(domainToDatabaseId.default)
 
 async function clientForScope(id) {
   const scopeDomain = scopeDomainCache[id]
   if (scopeDomain) return clientForDomain(scopeDomain)
   //  TODO: simultaneosly ask all clients for this id and return client that has it
-  await clientConnection
-  return client
+  return getConnection(domainToDatabaseId.default).then(({ client }) => client)
 }
 
 async function clientForDomain(domain) {
-  await clientConnection
-  return client
+  return getConnection(domainToDatabaseId.default).then(({ client }) => client)
 }
 
 async function setScope(id, path, state, options) {
@@ -79,20 +97,20 @@ async function scopeExists(id) {
 }
 
 async function subscribe(id, cb) {
-  //  TODO: choose subscription client to subscribe to based on location of server
-  await subscriptionsConnection
+  //  TODO: choose databaseId for connection based on location of server
+  const { subscriptions } = await getConnection(domainToDatabaseId.default)
   return subscriptions.subscribe(id, cb)
 }
 
 async function idsInDomain(domain) {
   //  TODO: find client where this set is set to be able to make this request
-  await clientConnection
+  const { client } = await getConnection(domainToDatabaseId.default)
   return client.sendCommand(['smembers', domain])
 }
 
 async function publish(id, message) {
   //  TODO: publish to all domain clients
-  await clientConnection
+  const { client } = await getConnection(domainToDatabaseId.default)
   return client.publish(id, message)
 }
 
