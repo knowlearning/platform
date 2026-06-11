5ae254b04e632e6e774e509a969c0a1126e7dac9
jasonford <fordjason@gmail.com>
2026-06-11 15:29:39 -0700

    Remove unused file

--- core/source/authenticate/oauth-client-info.js
+++
@@ -1,9 +0,0 @@
-import { environment } from '../utils.js'
-
-const { OAUTH_CREDENTIALS } = environment
-
-export default {
-  google: JSON.parse(GOOGLE_OAUTH_CLIENT_CREDENTIALS).web,
-  microsoft: JSON.parse(MICROSOFT_OAUTH_CLIENT_CREDENTIALS).web,
-  classlink: JSON.parse(CLASSLINK_OAUTH_CLIENT_CREDENTIALS).web
-}
